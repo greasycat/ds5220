99410b314505723538fc27cbe7f9c1e47677d45a
greasycat <131315c@gmail.com>
2025-03-10 18:40:15 -0400

    q3-partial

---
+++ ex1/q1.R
@@ -0,0 +1,87 @@
+# 1a
+install.packages("COUNT")
+data(loomis)
+str(loomis)
+
+# drop income and travel
+loomis$income <- NULL
+loomis$travel <- NULL
+
+# remove rows with NAs
+loomis_preped <- na.omit(loomis)
+
+str(loomis_preped)
+# count NAs
+
+# 1b
+# fit glm possion model
+glm_1b <- glm(anvisits ~ ., data = loomis_preped, family = poisson)
+
+summary(glm_1b)
+
+# Some values are NA in the summary because of the collinearity
+# which means that the design matrix is not full rank
+cat("Rank of the model matrix:", qr(glm_1b$model)$rank, "\n")
+cat("Coefficient number (with intercept):", length(coef(glm_1b)), "\n")
+
+# Rank of the model matrix: 8
+# Coefficient number: 9
+
+
+# copy data
+dat <- loomis_preped
+y <- dat[, 1] # dat is my data frame after completing 1 a
+# create transformed dummy variables
+# gender :
+gen <- dat$gender - 1
+# income :
+inc25p <- as.numeric(apply(dat[, c(4, 5, 6)], 1, function(x) 1 * (sum(x) > 0)))
+inc55p <- as.numeric(apply(dat[, c(5, 6)], 1, function(x) 1 * (sum(x) > 0)))
+inc95p <- dat[, 6]
+# travel
+tra025p <- as.numeric(apply(dat[, c(8, 9)], 1, function(x) 1 * (sum(x) > 0)))
+tra400p <- dat[, 9]
+# create data matrix
+
+X <- cbind( gen , inc25p , inc55p , inc95p , tra025p , tra400p )
+
+yX <- data.frame(cbind(y, X))
+# 1d
+# fit glm model
+glm_1d <- glm(y ~ ., family = poisson, data = yX)
+
+summary(glm_1d)
+
+# 1e CI of the coefficients
+
+coef <- glm_1d$coefficients
+coef
+se <- summary(glm_1d)$coefficients[, 2]
+
+# 95% CI
+ci <- cbind(coef - 1.96 * se, coef + 1.96 * se)
+ci
+
+# 1f
+
+
+cat(exp(coef[1] + coef[3] + coef[4]))
+cat(exp(coef[1] + coef[2] + coef[3] + coef[4]))
+
+# row c(0, 1, 1, 0, 0, 0))
+newdata <- data.frame(
+  gen = 0,
+  inc25p = 1,
+  inc55p = 1,
+  inc95p = 0,
+  tra025p = 0,
+  tra400p = 0
+)
+# copy data
+
+
+pred_2 <- predict(glm_1d, newdata = newdata, type = "response")
+
+pred_2
+
+ci
---
+++ ex1/q2.R
@@ -0,0 +1,59 @@
+source("ex1/q1.R")
+
+boot.fn <- function(data, index) {
+  d <- data[index, ]
+  model <- glm(y ~ ., family = poisson, data = d)
+
+  return (coef(model))
+}
+
+bootstrap <- function(data, boot.fn, R = 1000) {
+  initial_stat <- boot.fn(data, 1:nrow(data))
+  
+  stat_length <- length(initial_stat)
+  
+  boot_results <- matrix(NA, nrow = R, ncol = stat_length)
+  
+  # Perform bootstrap
+  for(i in 1:R) {
+    indices <- sample(nrow(data), replace = TRUE)
+    
+    boot_stat <- boot.fn(data, indices)
+    
+    boot_results[i, ] <- boot_stat
+  }
+  
+  boot_means <- colMeans(boot_results, na.rm = TRUE)
+  boot_se <- apply(boot_results, 2, sd, na.rm = TRUE)
+  
+  ci_lower <- apply(boot_results, 2, quantile, probs = 0.025, na.rm = TRUE)
+  ci_upper <- apply(boot_results, 2, quantile, probs = 0.975, na.rm = TRUE)
+
+  ci_width <- ci_upper - ci_lower
+  
+  bias <- boot_means - initial_stat
+  
+  # Prepare results
+  result <- data.frame(
+    bias = bias,                      # Bootstrap bias estimates
+    ci_lower=ci_lower,
+    ci_upper= ci_upper,
+    ci_width = ci_width
+    ) 
+  
+  names = colnames(data)[2:ncol(data)]
+  names = c("Intercept", names)
+  rownames(result) <- names
+
+  return(result)
+}
+
+bootstrap_results <- bootstrap(yX, boot.fn, R = 1000)
+
+options(digits=4)
+
+ci_width_1 = data.frame(ci_with_nb = ci[,2] - ci[,1])
+
+bootstrap_results <- cbind(bootstrap_results, ci_width_1)
+
+print(bootstrap_results)
